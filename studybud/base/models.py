963d7e72a6d1ad3c1e2d0a12937fc8a9534e3f19
rasool-ahad <rasoolahad1997.work@gmail.com>
2023-09-30 19:33:42 +0330

    working with templetes

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,17 +1,37 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 # Create your models here.
+class Topic(models.Model):
+    name = models.CharField(_("name"), max_length=200)
+    class Meta:
+        
+        verbose_name = 'topic'
+        verbose_name_plural = 'topics'
+    
+    def __str__(self):
+        return self.name
 class Room(models.Model):
-    # host =
-    # topic =
-    name = models.CharField( max_length=200)
-    description = models.TextField(null=True, blank=True)
+    host = models.ForeignKey(User, verbose_name=_("host"), on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey("base.Topic", verbose_name=_("topic"), on_delete=models.SET_NULL, null=True)
+    name = models.CharField(_("name"), max_length=200)
+    description = models.TextField(_("description"), null=True, blank=True)
     # participants =
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(_("update"), auto_now=True)
+    created = models.DateTimeField(_("create"), auto_now_add=True)
     def __str__(self):
         return self.name
     class Meta:
         db_table = 'Room'
         verbose_name = 'room'
         verbose_name_plural = 'rooms'
+
+class Message(models.Model):
+    user = models.ForeignKey(User, verbose_name=_("user"), on_delete=models.CASCADE)
+    room = models.ForeignKey("base.Room", verbose_name=_("room"), on_delete=models.CASCADE)
+    body = models.TextField(_("body"))
+    updated = models.DateTimeField(_("update"), auto_now=True)
+    created = models.DateTimeField(_("create"), auto_now_add=True)
+
+    def __str__(self):
+        return self.body[:50]
--- studybud/studybud/urls.py
+++ studybud/studybud/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
-def home(request):
-    print(request)
-    return HttpResponse("home page")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('home', include("base.urls"))
 ]
