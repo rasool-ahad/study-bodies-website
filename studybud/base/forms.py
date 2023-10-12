65a91cc55159292dc459716d6b3502cbebd3e8dd
rasool-ahad <rasoolahad1997.work@gmail.com>
2023-10-12 21:40:59 +0330

    user profile image finished

--- studybud/base/forms.py
+++ studybud/base/forms.py
@@ -7,7 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 class MyUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ["avatar",'name', 'username','email', 'password1', 'password2']
+        fields = ['name', 'username','email', 'password1', 'password2']
 class RoomForm(ModelForm):
     class Meta:
         model = Room
